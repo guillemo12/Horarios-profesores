06e17fbbd913135ed66fca7cb5b8037780cf6912
Guillermo <guillermoremesa@gmail.com>
2026-08-18 00:54:29 +0200

    chore(release): preparar version v0.0.3 con instalador en espanol

--- Web/src/updater.ts
+++ Web/src/updater.ts
@@ -1,6 +1,6 @@
 import { showToast } from './utils';
 
-export const CURRENT_VERSION = "0.0.1";
+export const CURRENT_VERSION = "0.0.3";
 export const GITHUB_REPO = "guillemo12/Horarios-profesores";
 
 export interface GitHubReleaseAsset {
